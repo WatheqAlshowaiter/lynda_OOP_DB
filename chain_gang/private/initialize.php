e649523f34d240736f9e83e3f361d2be37eb4d6d
Watheq <watheq.alshowaiter@gmail.com>
2019-12-18 10:44:24 +0300

    .DS_Store

--- chain_gang/private/initialize.php
+++ chain_gang/private/initialize.php
@@ -23,6 +23,7 @@ $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
 require_once('functions.php');
+require_once('status_error_functions.php');
 require_once('db_credentials.php');
 require_once('database_functions.php');
 
